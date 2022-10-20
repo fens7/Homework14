bba21c961133191a3aebb1f85346c35b2e5c8238
Maxym Zavadovych <zavadovich17@gmail.com>
2022-10-20 12:33:30 +0300

    optimization

--- main.js
+++ main.js
@@ -6,32 +6,13 @@ function passValid() {
     const value = passInput.value;
     const oneUpperLetter = new RegExp('(?=.*[A-Z])');
     const number = new RegExp('(?=.*[0-9])');
-    const specChar = new RegExp('(?=.*[!@#$%^&*])');
+    const specChar = new RegExp('(?=.*[-!.,:;?_"()])');
     const minLetters = new RegExp('(?=.{8,})');
 
-    if (oneUpperLetter.test(value)) {
-        li[0].style.color = 'green';
-    } else {
-        li[0].style.color = 'red';
-    }
-
-    if (number.test(value)) {
-        li[1].style.color = 'green';
-    } else {
-        li[1].style.color = 'red';
-    }
-
-    if (specChar.test(value)) {
-        li[2].style.color = 'green';
-    } else {
-        li[2].style.color = 'red';
-    }
-
-    if (minLetters.test(value)) {
-        li[3].style.color = 'green';
-    } else {
-        li[3].style.color = 'red';
-    }
+    li[0].style.color = oneUpperLetter.test(value) ? 'green' : 'red';
+    li[1].style.color = number.test(value) ? 'green' : 'red';
+    li[2].style.color = specChar.test(value) ? 'green' : 'red';
+    li[3].style.color = minLetters.test(value) ? 'green' : 'red';
 }
 
 passInput.oninput = passValid;
